bdc67766b70ab601c7120945b0be05ebf53f1aaf
yoranfu <yoranfu@gmail.com>
2019-10-12 12:04:41 +0800

    expose options for medium-zoom

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,9 +1,27 @@
 require([
   'gitbook'
 ], function(gitbook) {
-  var init = function () {
-    mediumZoom("img");
+  const options = {
+    margin: 0,
+    background: '#fff',
+    scrollOffset: 40
+  };
+
+  const init = function() {
+    mediumZoom("img", options);
   }
+
+  gitbook.events.bind('start', function(e, config){ 
+    const configOption = config['medium-zoom'];
+    if (configOption) {
+      for (const item in options) {
+        if (options.hasOwnProperty(item) && (item in configOption)) {
+          options[item] = configOption[item];
+        }
+      }
+    }
+  });
+
   gitbook.events.bind('page.change', function(e, config) {
     init();
   });
